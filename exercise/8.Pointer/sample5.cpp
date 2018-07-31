c18df0ca3b0e82abf24757f81c9db7196327bb55
shogo <fuji.arrows2556@gmail.com>
2018-08-01 03:59:26 +0900

    add L8

---
+++ exercise/8.Pointer/sample5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int* pA;
+	
+	a = 5;
+	pA = &a;
+
+	cout << "変数aの値は" << a << "です．\n";
+
+	*pA = 50;
+
+	cout << "*pAに50を代入しました．\n";
+	cout << "変数aの値は" << a << "です．\n";
+
+	return 0;
+}
---
+++ exercise/8.Pointer/sample9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void swap(int& rX, int& rY);
+
+int main()
+{
+	int num1 = 5;
+	int num2 = 10;
+
+	cout << "変数num1の値は" << num1 << "です．\n";
+	cout << "変数num2の値は" << num2 << "です．\n";
+	cout << "変数num1とnum2の値を交換します．\n";
+
+	swap(num1, num2);
+
+	cout << "変数num1の値は" << num1 << "です．\n";
+	cout << "変数num2の値は" << num2 << "です．\n";
+
+	return 0;
+}
+
+void swap(int& rX, int& rY)
+{
+	int tmp;
+
+	tmp = rX;
+	rX = rY;
+	rY = tmp;
+}
